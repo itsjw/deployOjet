24ff3419290f129bad9cbb9aa3fd128e3ec5e9cd
Sibabale <jojasibabale@gmail.com>
2017-11-23 09:22:40 +0200

    removed Procfile

--- index.js
+++ index.js
@@ -5,13 +5,9 @@ const express = require('express');
 
 // new instances
 const app = express();
-
 const port = process.env.PORT || 8083;
 
-
 app.use(express.static('Novemeber-Hackaton/web'));
-
-
 // listen for a localhost port and logging it on the console
 app.listen(port, function() {
   console.log('App running on port: ' + port);
